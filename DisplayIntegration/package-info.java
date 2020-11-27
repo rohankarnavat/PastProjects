7549c3e2827bb84765ddd76f7d5b375650eae845
Rohan Karnavat <44413775+rohankarnavat@users.noreply.github.com>
2020-11-27 13:43:05 -0600

    Add files via upload

---
+++ DisplayIntegration/package-info.java
@@ -0,0 +1,22 @@
+/*
+ *
+ * Copyright 2020 Rockwell Collins, Inc. All Rights Reserved
+ * NOTICE: The contents of this medium are proprietary to Rockwell
+ * Collins, Inc. and shall not be disclosed, disseminated, copied,
+ * or used except for purposes expressly authorized in written by
+ * Rockwell Collins, Inc.
+ *
+ */
+
+/**
+ * 
+ * Author: Rohan Karnavat
+ * Date: 04/26/2020
+ * 
+ * This package is made with the intent of integrating the Rosen Gulfstream monitor
+ * that runs on RS232. The monitor has a different msg structure than the
+ * Rosen made 15.6" monitor which also runs on RS232.
+ * 
+ *  */
+package com.rockwellcollins.cs.hcms.core.components.rglfm;
+
